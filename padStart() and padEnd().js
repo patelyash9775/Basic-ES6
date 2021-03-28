26a10f9229425e91bf4dfdc907a37320f02333e4
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-03-28 12:01:19 +0530

    padStart and padEnd functions

---
+++ padStart() and padEnd().js
@@ -0,0 +1,11 @@
+let example = 'Dylan';
+
+
+console.log(example.padStart(10, 'a'));
+console.log(example.padEnd(10, 'a'));
+
+
+let ex = 'YouTube.com/CodingTutorials360';
+
+console.log(ex.padStart(100));
+console.log(ex.padEnd(1));
