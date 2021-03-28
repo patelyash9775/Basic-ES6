a59be626e01e776aedee74b0b4fe21549c63f82d
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-03-28 11:58:53 +0530

    includes function

---
+++ includes().js
@@ -0,0 +1,9 @@
+
+const listIngredients =  [ "flour", "sugar", "eggs", "butter", 
+"chocolate" ];
+
+if( listIngredients.includes("chocolate")) {
+    console.log("We are going to make a chocolate cake" );
+} else {
+    console.log("We can't make a chocolate cake because we are missing the ingredient chocolate" );
+}
