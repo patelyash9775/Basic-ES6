6f33d69ff8981c59aa23cb7ac0db8fe4bee922a8
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-03-28 12:03:57 +0530

    Template-literal

---
+++ Template-literal.js
@@ -0,0 +1,12 @@
+let word1 = 'Patel';
+let word2 = 'Yash';
+let num1 = 2;
+let num2 = 3;
+
+// const fullName = `${word1} ${word2}`;
+let example = `${word1}
+${word2}
+`;
+// console.log(fullName);
+console.log(example)
+document.getElementById('example').innerText = example;
