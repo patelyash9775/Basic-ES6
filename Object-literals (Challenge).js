64deaa5b331d9e9331d429aa91b7031e5b470cc2
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-03-28 12:00:03 +0530

    Example of object-literals

---
+++ Object-literals (Challenge).js
@@ -0,0 +1,12 @@
+function addressMaker(address) {
+    const {city, state} = address;
+    
+    const newAddress = {
+        city,
+        state,
+        country: 'India'
+    };
+    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`);
+}
+
+addressMaker({city: 'Ahmedabad', state: 'Gujarat'});
