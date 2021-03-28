2ae4208bcbe291fca430b44e9cc0796af7b9a6a5
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-03-28 11:53:46 +0530

    Example of Class

---
+++ Classes (challenge).js
@@ -0,0 +1,30 @@
+class Player {
+    constructor(name, country) {
+        this.name = name;
+        this.country = country;
+    }
+    
+    introduce() {
+        console.log(`${this.name} was born in ${this.country}`);
+    }
+}
+
+const messi = new Player("Messi", "Argentina");
+
+messi.introduce();
+
+class TennisPlayer extends Player {
+    constructor(name, country, age) {
+        super(name, country);
+        this.age = age;
+    }
+    
+    playTennis() {
+        console.log(`${this.name} is ${this.age} years old and knows how to play Tennis`);
+    }
+}
+
+const roger = new TennisPlayer( "Roger Federer", "Spain", 38);
+
+roger.introduce();
+roger.playTennis();
